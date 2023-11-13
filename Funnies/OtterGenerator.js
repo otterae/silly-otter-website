a320165ee4516e4d64d09ce1ecefddcd95d16c77
Chin <86007652+Chin0303@users.noreply.github.com>
2023-11-13 13:21:15 +0100

    Not sure if works

---
+++ Funnies/OtterGenerator.js
@@ -0,0 +1,22 @@
+function generateRandomText() {
+    const filePath = 'Facts.txt';
+
+    fetch(filePath)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error fetching otter facts: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(text => {
+            const lines = text.split('\n');
+
+            const randomLineNumber = Math.floor(Math.random() * lines.length);
+            const randomLine = lines[randomLineNumber];
+
+            document.getElementById('Otterfact').innerText = randomLine;
+        })
+        .catch(error => {
+            console.error('An error occurred:', error.message);
+        });
+}
