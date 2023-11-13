c6950ddda645d0ea0730c593edffa177040c1f57
Chin <86007652+Chin0303@users.noreply.github.com>
2023-11-13 14:29:29 +0100

    Silly-fied it

--- Wordle/Wordle.js
+++ Wordle/Wordle.js
@@ -45,7 +45,7 @@ function intialize() {
     let keyboard = [
         ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", " "],
-        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "âŒ«" ]
+        ["Enter", "Z", "X", "C", "V", "B", "N", "M", "⌫" ]
     ]
 
     for (let i = 0; i < keyboard.length; i++) {
